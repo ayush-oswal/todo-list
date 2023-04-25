328a19eb25e67a9e4bbff0ae5e5037a3240604de
ayush-oswal <96043066+ayush-oswal@users.noreply.github.com>
2023-04-25 19:00:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 
-/* we have to set view engine as ejs. in this res.render function is used to send html pages.
-    it assumes that the page to be sent is an .ejs page in a folder named views.
-    any valid html is valid ejs. res.render is used to pass variable to .ejs files in key value pairs,
-    the first name is the same as used in .ejs file
-    and it can be accessed in the ejs file using <%=variable_name%>
-    //control statements can be written inside .ejs, have to be enclosed in <%....%>
-    express only serves main access point ie app.js and views folder by default, it ignores other static files.
-    so to render css and other js files we store them in a folder named public by convention.
-    and then write app.use(express.static("public"));
-    ejs layouts matlab joh bhi tumko repeated code chaiye woh ek file me daalo usko views wale folder me daalo and
-    use <%- include("file_name"); -%> aur samjo woh file ka code waha copy paste ho gya
-    we can also create our own module, require it using __dirname+"module_name",
-    we can export from module using exports keyword.
-    to export multiple things such as 2 or more functions use modules.exports.fun_name = fun,
-    due to this, the functions are exported as a whole object and can be accessed using . operator
-*/
 
 const express = require('express');
 
